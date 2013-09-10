a35595bb9cf851834b3fa91566e87eeae32ac6c1
ogagaga <y.ogagaga@gmail.com>
2013-09-10 16:08:20 +0900

    add all,new examples to Category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.all' do
+    before do
+      FactoryGirl.create(:category)
+    end
+    subject { Category.all }
+    it { should have(1).items }
+  end
+
+  describe '.new' do
+    context 'given valid attributes' do
+      subject { Category.new(:name => 'a') }
+      it { should be_valid }
+    end
+
+    context 'given null name' do
+      subject { Category.new(:name => '') }
+      it { should have(1).errors_on(:name) }
+    end
+  end
+
+  pending ".create, .edit, .destroy examples to (or delete) #{__FILE__}"
+  
 end
