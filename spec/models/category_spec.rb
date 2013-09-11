15d979b538a33dc55a98f6df38850c83b48477ce
ogagaga <y.ogagaga@gmail.com>
2013-09-11 14:10:30 +0900

    update category_spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Category do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  # https://github.com/thoughtbot/factory_girl/blob/master/GETTING_STARTED.md
+  # FactoryGirl.build()
+  #   - サンプルデータを作成
+  #   - 主に、バリデーションのテストなどに使い
+  # FactoryGirl.create(:hoge)
+  #   - サンプルデータを作成しテスト用のデータベースにデータをいれる
+  #   - バリデーションのテストではできない、重複禁止などのテストの時に使う。
+  # FactoryGirl.attributes_for(:user)
+  #   - 属性のハッシュを返却
 
   describe '.all' do
     before do
       FactoryGirl.create(:category)
     end
+    
     subject { Category.all }
     it { should have(1).items }
   end
@@ -23,6 +33,51 @@ describe Category do
     end
   end
 
-  pending ".create, .edit, .destroy examples to (or delete) #{__FILE__}"
-  
+  describe '.create' do
+    before do
+      @category = FactoryGirl.build(:category)
+    end
+    context 'given valid attributes' do
+      subject { @category.save }
+      it { should be_true }
+    end
+
+    context 'category was not saved' do
+      subject {
+        @category.name = ""
+        @category.save
+      }
+      it { should_not be_true }
+    end
+  end
+
+  describe '.update' do
+    before do
+      @category = FactoryGirl.build(:category)
+    end
+    context 'name of category was not changed' do
+      subject { @category.save }
+      it { should be_true }
+    end
+
+    context 'name of category was changed' do
+      subject {
+        @category.name = "a"
+        @category.save
+      }
+      it { should be_true }
+    end
+  end
+
+  describe '.destroy' do
+    before do
+      @category = FactoryGirl.create(:category)
+    end
+    context 'category was destroyed' do
+      subject { @category.destroy }
+      it { should be_true }
+    end
+
+  end
+
 end
