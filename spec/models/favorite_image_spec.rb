8fcd5dd6babae21fd9e6caa0da31005e62809438
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-09-12 21:50:38 +0900

    update spec/model favorite_image

--- spec/models/favorite_image_spec.rb
+++ spec/models/favorite_image_spec.rb
@@ -14,28 +14,78 @@ describe FavoriteImage do
   end
 
   describe '.new' do
-    before do
-      FactoryGirl.create(:category)
-      @favorite_image = FactoryGirl.build(:favorite_image)
-    end
 
     context 'given valid attributes' do
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+      end
       subject { @favorite_image }
       it { should be_valid }
     end
 
     context 'given nul title' do
-      subject { FavoriteImage.new(:title => '') }
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+        @favorite_image.title = ''
+      end
+      subject { @favorite_image }
       it { should_not be_valid }
+      subject { @favorite_image }
+      it { should have(1).errors_on(:title) }
     end
 
     context 'given null ownername' do
-      subject { FavoriteImage.new(:ownername => '') }
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+        @favorite_image.ownername = ''
+      end
+      subject { @favorite_image }
       it { should_not be_valid }
+      subject { @favorite_image }
+      it { should have(1).errors_on(:ownername) }
     end
-  end
 
-  # 下記はエラーが設定されているかを確認する書き方
-  # it { should have(1).errors_on(:name) }
+    context 'given null ownername' do
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+        @favorite_image.flickr_user_id = ''
+      end
+      subject { @favorite_image }
+      it { should_not be_valid }
+      subject { @favorite_image }
+      it { should have(1).errors_on(:flickr_user_id) }
+    end
+
+    context 'given null web_page_url' do
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+        @favorite_image.web_page_url = ''
+      end
+      subject { @favorite_image }
+      it { should_not be_valid }
+      subject { @favorite_image }
+      it { should have(1).errors_on(:web_page_url) }
+    end
+
+    context 'given null photo_source_url_n' do
+      before do
+        FactoryGirl.create(:category)
+        @favorite_image = FactoryGirl.build(:favorite_image)
+        @favorite_image.photo_source_url_n = ''
+      end
+      subject { @favorite_image }
+      it { should_not be_valid }
+
+      subject { @favorite_image }
+      it { should have(1).errors_on(:photo_source_url_n) }
+
+    end
+
+  end
 
 end
