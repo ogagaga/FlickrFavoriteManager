b8e0919f975a7eb1c2a269b2508e9f60371eeab1
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-09-12 13:24:38 +0900

    update favorite_image_spec

--- spec/models/favorite_image_spec.rb
+++ spec/models/favorite_image_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe FavoriteImage do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.all' do
+    before do
+      FactoryGirl.create(:favorite_image)
+      FactoryGirl.create(:category)
+    end
+
+    subject { FavoriteImage.all }
+    it { should have(1).items }
+  end
+
+
 end
