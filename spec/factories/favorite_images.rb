199bd7f463a5e95369461b88e5558c0432fc74b9
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-09-12 13:37:04 +0900

    update model spec of favorite_images

--- spec/factories/favorite_images.rb
+++ spec/factories/favorite_images.rb
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :favorite_image do
-    title "MyString"
-    ownername "MyString"
-    flickr_user_id "MyString"
-    photo_source_url_n "MyText"
+    category_id "1"
+    title "みたら室蘭　風車"
+    ownername "y_ogagaga"
+    flickr_user_id "50463420@N06"
+    web_page_url "http://www.flickr.com/photos/50463420@N06/5021249195"
+    photo_source_url_n "http://farm5.staticflickr.com/4154/5021249195_107bdd54d0.jpg"
   end
 end
