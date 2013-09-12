b323684cc4e77c9d609ae452098256cd272866d8
ogagaga <y.ogagaga@gmail.com>
2013-09-12 19:09:33 +0900

    update spec of favorite_image model

--- spec/models/favorite_image_spec.rb
+++ spec/models/favorite_image_spec.rb
@@ -1,16 +1,41 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe FavoriteImage do
 
   describe '.all' do
     before do
-      FactoryGirl.create(:favorite_image)
       FactoryGirl.create(:category)
+      FactoryGirl.create(:favorite_image)
     end
 
     subject { FavoriteImage.all }
     it { should have(1).items }
   end
 
+  describe '.new' do
+    before do
+      FactoryGirl.create(:category)
+      @favorite_image = FactoryGirl.build(:favorite_image)
+    end
+
+    context 'given valid attributes' do
+      subject { @favorite_image }
+      it { should be_valid }
+    end
+
+    context 'given nul title' do
+      subject { FavoriteImage.new(:title => '') }
+      it { should_not be_valid }
+    end
+
+    context 'given null ownername' do
+      subject { FavoriteImage.new(:ownername => '') }
+      it { should_not be_valid }
+    end
+  end
+
+  # 下記はエラーが設定されているかを確認する書き方
+  # it { should have(1).errors_on(:name) }
 
 end
