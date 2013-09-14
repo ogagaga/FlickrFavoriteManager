9dd06885d7d458f5b3c3a0479e0e8ff8a546194c
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-09-14 16:35:36 +0900

    update category factory

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
@@ -5,3 +6,23 @@ FactoryGirl.define do
     name "MyString"
   end
 end
+
+# 関連の書き方はこれだとだめらしい
+# # category --> favorite_image
+# FactoryGirl.define do
+#   factory :category_with_favorite_images, class: Category do
+#     name     { FactoryGirl.next(:category_name) }
+#     after_create do |category|
+#       favorite_images = [
+#         FactoryGirl(:favorite_image, :category_id => category.id),
+#         FactoryGirl(:favorite_image, :category_id => category.id)
+#       ]
+#     end
+#   end
+# end
+
+
+
+
+
+
