af6d6500240ba417508b600f92e97fe598007fb4
Yutaka Ogasawara <y.ogagaga@gmail.com>
2013-09-16 20:30:17 +0900

    remove code that does not use the SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,9 @@ class SearchController < ApplicationController
 
   def search
     begin
-    # logger.debug("params: #{params.inspect}")
-    unless params[:q].blank?
-      @discovered_pictures = flickr.photos.search(@args)
-      # logger.debug("#{@discovered_pictures.inspect}")
-    end
+      unless params[:q].blank?
+        @discovered_pictures = flickr.photos.search(@args)
+      end
     rescue
       logger.error "Flickr is not available"
     end
@@ -17,24 +15,11 @@ class SearchController < ApplicationController
 
   private
   def search_args
-    # TODO:検索オプションダイアログを作成する
-    #      何かJSのフレームワークで作ってみるかー。angular ?
     @args = {}
     @args[:text] = params[:q]
     # params[:q].gsub(/[\s　]/, "+")
     # 空白spあると自動でand検索になる。 "-"を付けると除外できる
     @args[:extras] = "owner_name"
-    # the default is street only granularity [16], which most images aren't...
-    @args[:accuracy] = 1 
-
-    # @args[:user_id] = params[:hoge]
-    # @args[:tags] = params[:hoge]
-    # @args[:] = params[:hoge]
-    # @args[:] = params[:hoge]
-    # @args[:] = params[:hoge]
-    # date-posted-desc	アップロード日時の新しい順
-    # date-taken-desc	撮影日時の新しい順
-    # license	ライセンス種別
   end
 
 end
